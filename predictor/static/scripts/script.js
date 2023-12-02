43bdcf41d798a47bbc9c0eecb579b2e03e19b93f
jithendra7043 <jithendrakavala@gmail.com>
2023-12-02 14:49:55 +0530

    Update script.js

--- predictor/static/scripts/script.js
+++ predictor/static/scripts/script.js
@@ -32,6 +32,7 @@ form.addEventListener("submit", (event) => {
     })
     .then(response => response.json())
     .then(data => {
+        print(data, searchKey)
         renderData(data, resultElement, searchKey)
     })
 })
